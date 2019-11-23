c6a425561d1cec7434a458dae16c21d8f4a46241
chanchira16 <57860708+chanchira16@users.noreply.github.com>
2019-11-23 15:05:11 +0700

    Update test10.go

--- test10.go
+++ test10.go
@@ -1,14 +1,1 @@
 package main
-
-import (
-	"fmt"
-	"strings"
-)
-
-fun main () {
-	ver b strings.Builder
-	b.WriteString("be")
-	b.WriteString(" ")
-	b.WriteString("janjira")
-	fmt.Println(b.String())
-}
