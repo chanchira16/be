a9297cdc9a69894d6b8f5ed45cb9f434c1e82960
chanchira16 <57860708+chanchira16@users.noreply.github.com>
2019-11-23 15:13:25 +0700

    Update test10.go

--- test10.go
+++ test10.go
@@ -1,1 +1,11 @@
 package main
+
+importn (
+	"fmt"
+	"string"
+)
+
+func main() {
+	fmt.Println(strings.HasSuffix("Hello World", "world"))
+	fmt.Println(strings.HasSuffix("Hello World",  "World"))
+}
