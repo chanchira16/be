b2b4ac2d363bbf6df69323ee7da0bfcfd6304b0d
chanchira16 <57860708+chanchira16@users.noreply.github.com>
2019-11-25 00:05:15 +0700

    Update test15.go

--- test15.go
+++ test15.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(3 + 2)
+	fmt.Println(3 + 8)
 	fmt.Println(3 - 2.199)
-	fmt.Println(3.0 * 2.0)
-	fmt.Println(3.0 / 2.0)
-	fmt.Println(3 % 2)
+	fmt.Println(5.0 * 2.74)
+	fmt.Println(4.0 / 2.0)
+	fmt.Println(5 % 2)
 }
