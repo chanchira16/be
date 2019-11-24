02494869eaa072481239c906d422257ac26479ec
chanchira16 <57860708+chanchira16@users.noreply.github.com>
2019-11-25 00:14:13 +0700

    Update test16.go

--- test16.go
+++ test16.go
@@ -6,6 +6,6 @@ func.main() {
 	fmt.Pirntln(23 + 100)
 	fmt.Pirntln(100 - 99)
 	fmt.Pirntln(25 * 1.5)
-	fmt.Pirntln(25 / 2.8)
-	fmt.Pirntln(2 % 2.5)
+	fmt.Pirntln(2.3 / 2.8)
+	fmt.Pirntln(2.3 % 2.5)
 }
