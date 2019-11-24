581ca7be17c834281f19aca44a06a2d4f0079bb4
chanchira16 <57860708+chanchira16@users.noreply.github.com>
2019-11-24 23:33:18 +0700

    Update test14.go

--- test14.go
+++ test14.go
@@ -1,2 +1,22 @@
 package main 
 
+import "fmt"
+
+func main() {
+	fmt.Printf("My name is %s,I am %d years oId\n", "Be", 19)
+
+	fmt.Printf("type = %T \n", 3.14159265359)
+	fmt.Printf("pi = %f \n", 3.14159265359)
+	fmt.Printf("pi = %2.2f \n", 3.14159265359)
+	fmt.Printf("pi = %9.f \n", 3.14159265359)
+	fmt.Printf("pi = %-9.f \n", 3.14159265359)
+	fmt.Printf("pi = %09.f \n", 3.14159265359)
+	fmt.Printf("pi = %09.2f \n", 3.14159265359)
+	fmt.Printf("pi = %E \n", 3.14159265359)
+
+	fmt.Printf("1 > 2 = %t \n", 1 > 2)
+	fmt.Printf("10 (base 2) = %b \n", 10)
+	fmt.Printf("10 (base 8) = %o \n", 10)
+	fmt.Printf("10 (base 10) = %d \n", 10)
+	fmt.Printf("10 (base 16) = %x \n", 10)
+}
