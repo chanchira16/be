5c248156e15bc121b2691d7f22972cde371100c9
chanchira16 <57860708+chanchira16@users.noreply.github.com>
2020-01-16 18:24:43 +0700

    Update test24.go

--- test24.go
+++ test24.go
@@ -3,4 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	n, e := fmt.println("hello", "world", 160, 743)
+	n, e := fmt.Println("hello", "world", 160, 743)
+	fmt.println("number of byter written :", n)
+	fmt.Println("write error encountered :", e)
