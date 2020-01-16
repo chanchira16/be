3f465cc9b87e7829b0cade24dcabea923ebc5db1
chanchira16 <57860708+chanchira16@users.noreply.github.com>
2020-01-17 01:31:43 +0700

    Update test27.go

--- test27.go
+++ test27.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var name string
 	var age int
-	n, e := fmt.scanin(&name, &age)
+	n, e := fmt.Scanin(&name, &age)
 	fmt.Println("name ", name)
 	fmt.Println("age ", age)
 	fmt.Println("nameber of items successfully scanned ", n)
