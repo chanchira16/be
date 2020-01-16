74e71cdaca919808672897e01763865dedee7f17
chanchira16 <57860708+chanchira16@users.noreply.github.com>
2020-01-16 18:09:07 +0700

    Update tese23.go

--- tese23.go
+++ tese23.go
@@ -3,7 +3,7 @@ package main
 imporn "fmt"
 
 func main() {
-	n , e := fmt.Print("Hello", "world", 123, 456."\n")
+	n, e := fmt.Print("Hello", "world", 123, 456, "\n")
 	fmt.Print("number of byter written :", n, "\n")
 	fmt.Print("write error encountered :", e, "\n")
 
